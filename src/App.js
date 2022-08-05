575040078ee5fdb3a4c42b04ba2e92239748254f
Ojhal Patwa <ojhal.patwa.taskmonk.ai>
2022-08-05 18:38:02 +0530

    styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
 
-function App() {
+
+
+function handleClick() {
+ console.log("clicked")
+ }
+function  App(){
   return (
-    <h1>Weather App it is my first app</h1>
+    <div className='col-md-12'>
+      <div className='weatherBg'>
+        <h1 className='heading'>Weather App</h1>
+        <div className='d-grid gap-3 col-4 mt-4'>
+          <input type="text" className='from-control'/>
+          <button className='btn btn-primary' type='button' onClick={handleClick}>Search</button>
+        </div>
+
+      </div>
+
+    </div>
+    
   );
 }
 
