e5c6c10657628b700e23efade89563a9c97e20c7
Ojhal Patwa <ojhal.patwa.taskmonk.ai>
2022-08-08 17:18:12 +0530

    using weather api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
+import img1 from "./weatherIcon.webp"
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 
 
-function handleClick() {
- console.log("clicked")
- }
+
 function  App(){
+
+const apiKey = "f13fba2800b1086c378e3a7b25805d1f"
+  const [cityName,setCityName] = useState("")
+  const[data, setData] = useState({})
+
+  const getWeatherDetails = (cityName) =>{
+    if(!cityName) return
+    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey
+    axios.get(apiUrl).then((res) =>{
+      setData(res.data)
+      console.log("res",res.data)
+    }).catch((err)=>{
+      console.log("err",err)
+    })
+  }
+  const handleChange = (e) =>{
+    // console.log("value",e.target.value)
+    setCityName(e.target.value)
+  }
+
+  const handleSearch = () => {
+    getWeatherDetails(cityName)
+    // console.log("clicked")
+    }
+
+  // useEffect(() => {
+  //   getWeatherDetails("delhi")
+  // },[])
   return (
     <div className='col-md-12'>
       <div className='weatherBg'>
         <h1 className='heading'>Weather App</h1>
         <div className='d-grid gap-3 col-4 mt-4'>
-          <input type="text" className='from-control'/>
-          <button className='btn btn-primary' type='button' onClick={handleClick}>Search</button>
+          <input type="text" placeholder="Enter city name"className='from-control' 
+          value={cityName}
+          onChange={handleChange}/>
+          <button className='btn btn-primary' type='button'
+           onClick={handleSearch}>Search</button>
         </div>
-
-      </div>
+        </div>
+        <div className='col-md-12 text-center mt-5'>
+         <div className='shadow rounded weatherResultBox'>
+          <img className='weatherIcon' src={img1} alt=''/>
+          <h5 className='weatherCity'>{data.name}</h5>
+          <h6 className='weatherTemp'>{((data.main.temp) - 273.15).toFixed(2)} °C</h6>
+          </div> 
+        </div>
 
     </div>
     
